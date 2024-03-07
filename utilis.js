a866632547aba955b02d043ffe116b7b2680b5f0
Manpreet <hcmanpreet2005@gmail.com>
2024-03-07 12:59:04 -0600

    start project

---
+++ utilis.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Get HTML element by id
+export function getElement(selector, scope = document) {
+    return scope.getElementById(selector);
+  }
+  
+  // Select HTML element
+  export function select(selector, scope = document) {
+    return scope.querySelector(selector);
+  }
+  
+  // Get a list of HTML elements as an array
+  export function selectAll(selector, scope = document) {
+    return [...scope.querySelectorAll(selector)];
+  }
+  
