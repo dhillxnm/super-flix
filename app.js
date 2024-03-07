3c26c053586dd84852377f820f2e4a5f3d3d053e
Manpreet <hcmanpreet2005@gmail.com>
2024-03-07 13:35:43 -0600

    add variables

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
-let message = 'My second feature';
+let myMessage = 'My second feature';
 let year = 2025;
 let weather = 'Snow apocalypse';
+let id = 4455454;
+let city = 'winnipeg'; 
